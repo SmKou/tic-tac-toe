410c4981a44cdcfa684bcc4dd7052f67110e316d
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-15 15:06:28 -0700

    Add win logic
    Co-authored-by: Thomas Bakken <tbakken14@users.noreply.github.com>

--- js/game.js
+++ js/game.js
@@ -1,8 +1,9 @@
 class GameState {
     constructor() {
         this.board = [];
+        this.wins = [0, 0];
+        this.isXTurn;
         this.init();
-        this.isXTurn = true;
     }
     
     init() {
@@ -11,48 +12,30 @@ class GameState {
             this.board[x] = [];
             for (let y = 0; y < 3; y++)
                 this.board[x][y] = '-';
-        }  
+        } 
+        this.isXTurn = 1;
     }
 
     makeMove(position) {
         const [x, y] = position;
-        this.board[x][y] = this.isXturn ? "x" : "o";
+        this.board[x][y] = this.isXTurn ? "x" : "o";
     }
 
-    checkForPlayer(player) {
-        return player ? 'x' : 'o'
+    checkForPlayer() {
+        return this.isXTurn ? 'x' : 'o'
     } 
 
-    /*
-    NOTE: Removed || statement bc would return true regardless of player as long as row, column or diagonal was filled
-    - Added player
-    - Not sure about diagonal
-    */
     isWon(movePosition) {
         const [x, y] = movePosition;
-        const player = checkForPlayer(this.isXTurn);
-        //check row
-        if (this.board[x].every((e) => (e === player))) {
+        const player = this.checkForPlayer();
+        if (this.board[x].every((e) => e === player)
+            || this.board.every((e) => e[y] === player)
+            || this.board.every((e, i) => e[i] === player)
+            || this.board.every((e, i) => e[2 - i] === player)) {
+            this.wins[this.isXTurn]++;
             return true;
         }
-        //check column
-        if (this.board.every((e) => e[y] === player)) {
-            return true;
-        }
-
-        //check diagonals (if on a diagonal)
-        if (x === y) {
-            if (this.board.every((e, i) => e[i] === "X") || 
-                    this.board.every((e, i) => e[i] === "O")) {
-                return true;
-            }
-        }
-        if (x + y === 2) {
-            if (this.board.every((e, i) => e[2 - i] === "X") || 
-                    this.board.every((e, i) => e[2 - i] === "O")) {
-                return true;
-            }
-        }
+        return false;
     }
 }
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,40 @@
 const gameboard = document.querySelector('.gameboard');
 const warning = document.querySelector('.warning');
+const restart = document.querySelector('#restart-btn');
+
 const handleGameBoard = (e) => {
     const eId = e.target.id;
     const square = document.querySelector('#' + eId);
     const [x, y] = eId
         .split('-')
         .map(c => parseInt(c[1]) - 1);
+    
     warning.innerText = '';
     if (!square.classList.contains('x') && !square.classList.contains('o'))
         square.classList.add(controller.isXTurn ? 'x' : 'o');
     else
-        warning.innerText = 'Already taken. Player ' + (Number(controller.isXTurn) + 1) + ', you lose your turn.'
-    controller.isXTurn = !controller.isXTurn;
+        warning.innerText = 'Already taken. Player ' + controller.checkForPlayer().toUpperCase() + ', you lose your turn.'
     controller.makeMove([x, y]);
     const winner = controller.isWon([x, y]);
     if (winner) {
-        warning.innerText = 'The winner is Player ' + Number(controller.isXTurn);
-        console.log(winner)
+        warning.innerText = 'The winner is Player ' + controller.checkForPlayer().toUpperCase();
+        restart.classList.add('active');
+        gameboard.onclick = null;
     }
+    else
+        controller.isXTurn = !controller.isXTurn;
 }
 gameboard.onclick = handleGameBoard;
 
-document
-    .querySelector('#restart-btn')
-    .addEventListener('click', () => {
-        controller.init();
-        warning.innerText = '';
-        gameboard.onclick = handleGameBoard;
-        document.querySelectorAll('square').forEach(s => {
-            if (s.classList.contains('x'))
-                s.classList.remove('x');
-            if (s.classList.contains('o'))
-                s.classList.remove('o');
-        })
-    })
+restart.addEventListener('click', () => {
+    controller.init();
+    warning.innerText = '';
+    restart.classList.remove('active');
+    gameboard.onclick = handleGameBoard;
+    document.querySelectorAll('.square').forEach(squ => {
+        if (squ.classList.contains('x'))
+            squ.classList.remove('x');
+        if (squ.classList.contains('o'))
+            squ.classList.remove('o');
+    });
+})
